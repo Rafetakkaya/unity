cb141967d54f02e0e79d5c06d8d07167cd002163
Rafet Akkaya <akkayaraf@gmail.com>
2023-11-20 22:25:52 +0300

    ok
    ok

---
+++ Assets/text.cs
@@ -0,0 +1,25 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using TMPro;
+//using UnityEngine;
+
+//public class text : MonoBehaviour
+//{
+//    private TextMeshPro textMeshPro;
+//    void Start()
+//    {
+//        textMeshPro= GetComponent<TextMeshPro>();
+//        textMeshPro.text = "";
+        
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        if(Input.anyKey) {
+//            textMeshPro.text = textMeshPro.text + Input.inputString;
+        
+//        }
+        
+//    }
+//}
